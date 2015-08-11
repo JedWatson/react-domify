6f2daaaaab86ed55a64a1e2cb1cced34c224b7a8
Jed Watson <jed.watson@me.com>
2015-08-11 13:43:34 +1000

    Updating build

--- dist/react-domify.js
+++ dist/react-domify.js
@@ -1,7 +1,8 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.DOMify = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
 'use strict';
 
-var React = (window.React);
+var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
 
 var styles = {
 	string: {
@@ -32,91 +33,98 @@ var styles = {
 	object: {
 		color: '#0b89b6',
 		cursor: 'default'
+	},
+	comma: {
+		color: '#999999',
+		cursor: 'default'
 	}
 };
 
-function transform(obj, fromRecur) {
+function transform(obj, fromRecur, comma) {
 
 	var tag = fromRecur ? 'span' : 'div';
 	var nextLevel = (fromRecur || 0) + 1;
 	var children = [];
+	comma = comma ? React.createElement(
+		'span',
+		{ style: styles.comma },
+		','
+	) : null;
 
 	// strings
 	if (typeof obj === 'string') {
-		return React.createElement(tag, { style: styles.string }, obj);
+		return React.createElement(tag, { style: styles.string }, obj, comma);
 	}
 	// booleans, null and undefined
 	else if (typeof obj === 'boolean' || obj === null || obj === undefined) {
-		return React.createElement(tag, { style: styles.bool }, String(obj));
-	}
-	// numbers
-	else if (typeof obj === 'number') {
-		return React.createElement(tag, { style: styles.number }, String(obj));
-	}
-	// dates
-	else if (Object.prototype.toString.call(obj) === '[object Date]') {
-		return React.createElement(tag, { style: styles.date }, String(obj));
-	}
-	// arrays
-	else if (Array.isArray(obj)) {
-
-		if (!obj.length) {
-			return React.createElement(tag, { style: styles.empty }, 'Array: []');
+			return React.createElement(tag, { style: styles.bool }, String(obj), comma);
 		}
-
-		children.push(React.createElement(tag, { key: '__array:open__', style: styles.array }, 'Array: ['));
-
-		// rtn += '</' + tag + '><div style="padding-left: 20px;">';
-		//
-		for (var i = 0; i < obj.length; i++) {
-			children.push(React.createElement(
-				'div',
-				{ key: 'i' + i, style: { paddingLeft: '20px' } },
-				transform(obj[i], nextLevel),
-				i < obj.length - 1 ? ',' : null
-			));
-		}
-
-		children.push(React.createElement(tag, { key: '__array:close__', style: styles.array }, ']'));
-	}
-	// objects
-	else if (obj && typeof obj === 'object') {
-
-		var len = Object.keys(obj).length;
-
-		if (fromRecur && !len) {
-			return React.createElement(tag, { style: styles.empty }, 'Object: {}');
-		}
-
-		if (fromRecur) {
-			children.push(React.createElement(tag, { key: '__object:open__', style: styles.object }, 'Object: {'));
-		}
-
-		for (var key in obj) {
-			if (typeof obj[key] !== 'function') {
-				children.push(React.createElement(
-					'div',
-					{ key: key, style: { paddingLeft: fromRecur ? '20px' : '0' } },
-					React.createElement(
-						'span',
-						{ style: { paddingRight: '5px', cursor: 'default' } },
-						key,
-						':'
-					),
-					transform(obj[key], nextLevel)
-				));
+		// numbers
+		else if (typeof obj === 'number') {
+				return React.createElement(tag, { style: styles.number }, String(obj), comma);
 			}
-		}
+			// dates
+			else if (Object.prototype.toString.call(obj) === '[object Date]') {
+					return React.createElement(tag, { style: styles.date }, String(obj), comma);
+				}
+				// arrays
+				else if (Array.isArray(obj)) {
 
-		if (fromRecur) {
-			children.push(React.createElement(tag, { key: '__object:close__', style: styles.object }, '}'));
-		}
-	}
+						if (!obj.length) {
+							return React.createElement(tag, { style: styles.empty }, 'Array: []');
+						}
+
+						children.push(React.createElement(tag, { key: '__array:open__', style: styles.array }, 'Array: ['));
+
+						for (var i = 0; i < obj.length; i++) {
+							children.push(React.createElement(
+								'div',
+								{ key: 'i' + i, style: { paddingLeft: '20px' } },
+								transform(obj[i], nextLevel, i < obj.length - 1)
+							));
+						}
+
+						children.push(React.createElement(tag, { key: '__array:close__', style: styles.array }, ']'));
+					}
+					// objects
+					else if (obj && typeof obj === 'object') {
+
+							var len = Object.keys(obj).length;
+
+							if (fromRecur && !len) {
+								return React.createElement(tag, { style: styles.empty }, 'Object: {}');
+							}
+
+							if (fromRecur) {
+								children.push(React.createElement(tag, { key: '__object:open__', style: styles.object }, 'Object: {'));
+							}
+
+							for (var key in obj) {
+								if (typeof obj[key] !== 'function') {
+									children.push(React.createElement(
+										'div',
+										{ key: key, style: { paddingLeft: fromRecur ? '20px' : '0' } },
+										React.createElement(
+											'span',
+											{ style: { paddingRight: '5px', cursor: 'default' } },
+											key,
+											':'
+										),
+										transform(obj[key], nextLevel)
+									));
+								}
+							}
+
+							if (fromRecur) {
+								children.push(React.createElement(tag, { key: '__object:close__', style: styles.object }, '}'));
+							}
+						}
 
 	return React.createElement(
 		'div',
 		null,
-		children
+		children,
+		comma
 	);
 }
 
@@ -130,5 +138,6 @@ var DOMify = React.createClass({
 
 module.exports = DOMify;
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}]},{},[1])(1)
 });
